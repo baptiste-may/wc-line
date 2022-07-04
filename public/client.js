210c417d3b681ecdba461b31774fca135dca0986
DjRedstone <djredstone64@gmail.com>
2022-07-04 13:54:10 +0200

    Ajout d'une fenêtre pour avoir le nom de l'utilisateur

--- public/client.js
+++ public/client.js
@@ -1,3 +1,15 @@
 
 const socket = io();
 
+if (Cookies.get("name") != undefined) {
+    $("#ask-name").remove();
+}
+$("#ask-name-form").on("submit", (e) => {
+    e.preventDefault();
+
+    Cookies.set("name", $("#name").val().replace(" ", "-"));
+    $("#ask-name").css("opacity", 0);
+    setTimeout(() => {
+        $("#ask-name").remove();
+    }, 500);
+});
